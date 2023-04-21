87d626d612310c313d5babbd96299242ff9cb41c
Taeyooooon <eric568@naver.com>
2023-04-21 19:20:53 +0900

    Style: fix nav icon bug

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { BiWifi, BiWifiOff } from 'react-icons/bi';
 export default function Nav() {
   return (
     <header className='w-full bg-red-300 py-2 px-4 '>
-      <span className=' ml-20 text-xl font-bold'>
+      <span className=' text-xl font-bold'>
         {socket.connected ? <BiWifi /> : <BiWifiOff />}
       </span>
     </header>
