13925b93843a0d017b413918afa7572a9eac9d05
Taeyooooon <eric568@naver.com>
2023-04-21 19:19:00 +0900

    Style: nav 웹소켓 연결상태 아이콘, favicon 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,12 @@
 import { socket } from '../Router';
+import { BiWifi, BiWifiOff } from 'react-icons/bi';
 
 export default function Nav() {
   return (
-    <header className='w-full bg-red-300 p-2 flex justify-center'>
-      <div>웹소켓 {socket.connected ? '연결됨' : '연결안됨'}</div>
+    <header className='w-full bg-red-300 py-2 px-4 '>
+      <span className=' ml-20 text-xl font-bold'>
+        {socket.connected ? <BiWifi /> : <BiWifiOff />}
+      </span>
     </header>
   );
 }
